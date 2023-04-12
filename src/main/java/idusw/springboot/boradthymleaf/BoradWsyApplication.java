7f329bb76f6d32cf08a82a79f90c1e275e0b5ce9
woosangz <tkdduq1737@gmail.com>
2023-04-12 16:47:26 +0900

    0412

--- src/main/java/idusw/springboot/boradthymleaf/BoradWsyApplication.java
+++ src/main/java/idusw/springboot/boradthymleaf/BoradWsyApplication.java
@@ -2,13 +2,12 @@ package idusw.springboot.boradthymleaf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication  //(exclude = DataSourceAutoConfiguration.class)
-public class BoradThymleafApplication {
+public class BoradWsyApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BoradThymleafApplication.class, args);
+        SpringApplication.run(BoradWsyApplication.class, args);
     }
 
 }
--- src/main/java/idusw/springboot/boradthymleaf/controller/HomeController.java
+++ src/main/java/idusw/springboot/boradthymleaf/controller/HomeController.java
@@ -4,14 +4,24 @@ import idusw.springboot.boradthymleaf.service.MemoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/sb-admin-2")
 public class HomeController {
     @Autowired
     MemoService memoService;  // MemoService 인터페이스의 구현체를 필드 주입
 
     @GetMapping("/")
     public String goHome() {
-        return "index";
+        return "/sb-admin-2/index";
+    }
+    @GetMapping("buttons")
+    public String goButton() {
+        return "/sb-admin-2/buttons";
+    }
+    @GetMapping("cards")
+    public String goCard() {
+        return "/sb-admin-2/cards";
     }
 }
